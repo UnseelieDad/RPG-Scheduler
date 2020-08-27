68b0fd7009505c4e2162212a551d990a584524a3
Seth Martin <voiceofsethmartin@gmail.com>
2020-08-27 18:18:49 -0500

    working on moving auth to backend

--- functions/auth.js
+++ functions/auth.js
@@ -17,7 +17,7 @@ router.post("/", (req, res) => {
   })
 })
 
-router.get("/login", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({
     message: "Hello World!",
   })
@@ -27,3 +27,36 @@ app.use(bodyParser.json())
 app.use("/.netlify/functions/auth", router)
 
 module.exports.handler = serverless(app)
+
+// set up a data object for the auth request
+// const data = {
+//   client_id: process.env.GATSBY_DISCORD_CLIENT_ID,
+//   client_secret: process.env.GATSBY_DISCORD_CLIENT_SECRET,
+//   grant_type: "authorization_code",
+//   redirect_uri: "http://localhost:8000/app",
+//   code: code,
+//   scope: "identify",
+// }
+// // get the auth token
+// fetch("https://discord.com/api/oauth2/token", {
+//   method: "POST",
+//   body: new URLSearchParams(data),
+//   headers: {
+//     "Content-Type": "application/x-www-form-urlencoded",
+//   },
+// })
+//   .then(response => {
+//     return response.json()
+//   })
+//   // use auth data to fetch user
+//   .then(responseData =>
+//     fetch("https://discordapp.com/api/users/@me", {
+//       headers: {
+//         authorization: `${responseData.token_type} ${responseData.access_token}`,
+//       },
+//     })
+//   )
+//   .then(res => res.json())
+//   .then(resData => {
+//     setUserName(resData.username)
+//   })
---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+const Auth = ({ location }) => {
+  const [username, setUserName] = useState("")
+
+  useEffect(() => {
+    const code = props.location.search.split("=")[1]
+
+    axios
+      .post({
+        code: code,
+      })
+      .then(res => {
+        setUserName(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [location])
+
+  return <p>{username !== "" ? `Hello ${username}!` : "Loading..."}</p>
+}
+
+export default Auth
