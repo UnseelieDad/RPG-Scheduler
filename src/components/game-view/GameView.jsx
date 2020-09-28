9835b28ee344a6de6c3f40f254d5fe7d227faf8a
UnseelieDad <voiceofsethmartin@gmail.com>
2020-09-28 18:48:35 -0500

    More notes

--- src/components/game-view/GameView.jsx
+++ src/components/game-view/GameView.jsx
@@ -6,6 +6,7 @@ import { Link } from "gatsby"
 import GameList from "./game-list/GameList"
 import gameViewStyles from "./gameViewStyles"
 const GameView = () => {
+  // If the date is today's date change the time from numbers to a countdown
   const classes = gameViewStyles()
   return (
     <Container className={classes.root}>
